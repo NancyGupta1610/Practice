28afd644f1c5e8e898cc318e3fda5c5d4bc8c998
NancyGupta1610 <nancygupta1616g@gmail.com>
2020-12-21 13:57:13 +0530

    First Commit

---
+++ src/test/java/com/Naina/MyFirstBootApp/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Naina.MyFirstBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
